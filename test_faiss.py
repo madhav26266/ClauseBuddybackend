1e58ee7541c936dd5436c96a0604cfd52b83d108
Madhav Singh <138430221+madhav26266@users.noreply.github.com>
2025-09-22 12:51:49 +0530

    Delete test_faiss.py

--- test_faiss.py
+++
@@ -1,8 +0,0 @@
-import faiss
-
-print("FAISS version:", faiss.__version__)
-
-# Optional: simple test to create an index
-dim = 768  # embedding dimension (same as InLegalBERT)
-index = faiss.IndexFlatL2(dim)
-print("Index created. Is trained?", index.is_trained)
